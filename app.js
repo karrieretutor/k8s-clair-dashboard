3fbeeefdbae8f9f8be5a1c8f262ab050cba10a77
kevin Seidel <kevin.seidel@karrieretutor.de>
2018-11-12 17:38:34 +0100

    added basic css, index enhanced

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(express.static('assets'));
 
 console.log('Starting up...');
 
@@ -15,9 +16,16 @@ var images;
 k8s.listAllImages()
   .then( (img) => {
     images = img;
+    images = images.sort();
   })
   .catch( (err) => {
     console.log(err);
   });
 
+app.get('/', (req, res) => {
+  res.render('index', {images: images});
+});
 
+app.listen(8080, () => {
+  console.log('listening on 8080');
+});
