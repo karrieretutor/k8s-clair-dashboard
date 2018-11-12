70e1365affd0b76f8585066204560afdea942520
kevin Seidel <kevin.seidel@karrieretutor.de>
2018-11-12 17:13:00 +0100

    added *some* stuff to app and added pug as dep

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const app = require('express');
+const express = require('express');
 const clair = require('./modules/clairclient');
 const Report = require('./modules/report').Report;
+const k8s = require('./modules/kubeclient');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+console.log('Starting up...');
 
 var report = new Report();
+
+var images;
+k8s.listAllImages()
+  .then( (img) => {
+    images = img;
+  })
+  .catch( (err) => {
+    console.log(err);
+  });
+
+
