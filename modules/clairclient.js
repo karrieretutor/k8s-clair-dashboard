b819798eb237b460b1a0ad78442ddb26da444d33
Kevin Seidel <kevin.seidel@karrieretutor.de>
2018-11-13 16:54:36 +0100

    validating docker cred input at least somewhat

--- modules/clairclient.js
+++ modules/clairclient.js
@@ -1,15 +1,17 @@
 const Clair  = require('clair-client');
+const assert = require('assert');
 const clairAddress = process.env.CLAIR_ADDRESS || 'http://localhost:6060';
 
 try {
   //check if we have to deal with a priv registry
   if ( process.env.USE_PRIV_REGISTRY ) {
-    let dockerUname = process.env.DOCKER_USER ||
-      reject("Trying to use priv. Docker registry but \
-        no Docker user name set");
-    let dockerPass = process.env.DOCKER_PASS ||
-      reject("Trying to use priv. Docer registry but \
-        no Docker password set");
+    let dockerUname = process.env.DOCKER_USER
+    let dockerPass = process.env.DOCKER_PASS 
+
+    //check if Uname and pw are provided
+    assert.ok(dockerUname, 'DOCKER_USER required but not provided');
+    assert.ok(dockerPass, 'DOCKER_PASS required but not provided');
+
     var clair = new Clair({ clairAddress, dockerUname, dockerPass});
   }
   else {
