d0e265f74ecf0b8f67efc6e3afcb119c16404c87
Kevin Seidel <kevin.seidel@karrieretutor.de>
2018-11-13 11:57:14 +0100

    added option for priv docker reg in clairclient

--- modules/clairclient.js
+++ modules/clairclient.js
@@ -4,13 +4,28 @@ const Clair  = require('clair-client');
 const analyze = function ( image ) {
   return new Promise ( (resolve, reject) => {
     const clairAddress = process.env.CLAIR_ADDRESS || 'http://localhost:6060';
+
     try {
-      const clair = new Clair({ clairAddress });
+      //check if we have to deal with a priv registry
+      if ( process.env.USE_PRIV_REGISTRY ) {
+        var dockerUname = process.env.DOCKER_USER ||
+          reject("Trying to use priv. Docker registry but \
+            no Docker user name set");
+        var dockerPass = process.env.DOCKER_PASS ||
+          reject("Trying to use priv. Docer registry but \
+            no Docker password set");
+        var clair = new Clair({ clairAddress, dockerUname, dockerPass});
+      }
+      else {
+        var clair = new Clair({ clairAddress });
+      }
+
       var analysis = clair.analyze({ image: image });
     }
     catch (err) {
       reject(err);
     }
+    
     resolve( analysis );
   });
 }
